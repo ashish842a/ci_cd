2ab2c6bcaac20e92379cb9da00afb2f451b41196
Admin <ashishkumar0842a@gmail.com>
2026-02-12 15:11:51 +0530

    correct the vite config

--- vite.config.js
+++ vite.config.js
@@ -11,27 +11,42 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-
 export default defineConfig(({ mode }) => {
 
-  let basePath = '/ci_cd/'
-
-  if (mode === 'development') {
-    basePath = '/'
-  }
-
-  if (mode === 'dev') {
-    basePath = '/ci_cd/dev/'
-  }
-
-  if (mode === 'staging') {
-    basePath = '/ci_cd/staging/'
+  const basePaths = {
+    dev: '/ci_cd/dev/',
+    staging: '/ci_cd/staging/',
+    production: '/ci_cd/',
   }
 
   return {
     plugins: [react()],
-    base: basePath,
+    base: basePaths[mode] || '/',
   }
 })
+
+
+
+// export default defineConfig(({ mode }) => {
+
+//   let basePath = '/ci_cd/'
+
+//   if (mode === 'development') {
+//     basePath = '/'
+//   }
+
+//   if (mode === 'dev') {
+//     basePath = '/ci_cd/dev/'
+//   }
+
+//   if (mode === 'staging') {
+//     basePath = '/ci_cd/staging/'
+//   }
+
+//   return {
+//     plugins: [react()],
+//     base: basePath,
+//   }
+// })
 
 
